3739a4cbaa24905d17aafbe965a1171ea5557434
Anthony Chavez <artisgarbage@gmail.com>
2016-02-23 15:02:20 -0700

    PUT users/:ID and cleanup, nicer logs

--- Server.js
+++ Server.js
@@ -9,16 +9,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({"extended" : false}));
 
 router.get("/",function(req,res){
-    res.json({"error" : false, "message" : "Hello Black & Red, Inc."});
+  res.json({"error" : false, "message" : "Hello Black & Red, Inc."});
 });
 
 router.route("/users")
   .get(function(req,res){
     var response = {};
-
     mongoOp.find({},function(err,data){
       if(err) {
-        response = {"error" : true, "message" : "Error retreiving user data"};
+        response = {"error" : true, "message" : "Error retrieving user data"};
       } else {
         response = {"error" : false, "message" : data};
       }
@@ -30,15 +29,16 @@ router.route("/users")
     var response = {};
 
     db.usrEmail = req.body.email;
+    // Base 64 hash password transmission
     db.usrPass = require('crypto')
       .createHash('sha1')
       .update(req.body.password)
       .digest('base64');
     db.save(function(err){
       if(err) {
-          response = {"error" : true, "message" : "Error adding user data"};
+        response = {"error" : true, "message" : "Error adding user data"};
       } else {
-          response = {"error" : false, "message" : "User data added"};
+        response = {"error" : false, "message" : "User data added"};
       }
       res.json(response);
     });
@@ -50,11 +50,37 @@ router.route("/users/:id")
     // Query users by ID
     mongoOp.findById(req.params.id,function(err,data){
       if(err) {
-          response = {"error" : true, "message" : "Error fetching data"};
+        response = {"error" : true, "message" : "Error retrieving user data for" + req.params.id};
       } else {
-          response = {"error" : false, "message" : data};
+        response = {"error" : false, "message" : data};
       }
       res.json(response);
+    });
+  })
+  .put(function(req,res){
+    var response = {};
+    mongoOp.findById(req.params.id,function(err,data){
+      if(err) {
+        response = {"error" : true,"message" : "Error retrieving user data for" + req.params.id};
+      } else {
+        // Update email
+        if(req.body.userEmail !== undefined){
+          data.userEmail = req.body.userEmail;
+        }
+        // Update PW
+        if(req.body.userPassword !== undefined){
+          data.userPassword = req.body.userPassword;
+        }
+        // Save record
+        data.save(function(err){
+          if(err) {
+              response = {"error" : true, "message" : "Error updating user data for " + req.params.id};
+          } else {
+              response = {"error" : false, "message" : "User data is updated for " + req.params.id};
+          }
+          res.json(response);
+        })
+      }
     });
   });
 
