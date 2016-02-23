6c11473226fe58e51858ab54217377917e020ad6
Anthony Chavez <artisgarbage@gmail.com>
2016-02-22 19:47:04 -0700

    Server.js

---
+++ Server.js
@@ -0,0 +1,17 @@
+
+var express     =   require("express");
+var app         =   express();
+var bodyParser  =   require("body-parser");
+var router      =   express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({"extended" : false}));
+
+router.get("/",function(req,res){
+    res.json({"error" : false,"message" : "Hello Black & Red, Inc."});
+});
+
+app.use('/',router);
+
+app.listen(3000);
+console.log("Listening to PORT 3000");
